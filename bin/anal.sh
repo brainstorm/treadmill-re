2362fa8b1171f00773e755f246067f16e8e1f47b
Roman Valls Guimera <brainstorm@users.noreply.github.com>
2024-10-16 15:03:48 +1100

    Analyse control r2script

--- bin/anal.sh
+++ bin/anal.sh
@@ -5,6 +5,6 @@ R2_CACHEDIR=$PWD/.r2project/cache
 # Convert function exports from Ghidra
 python3 ./bin/ghidra_labels_to_r2_flags.py
 
-#r2 -a stm8 -n -i r2/import_flags.r2 -i r2/anal.r2 -m 0x8000 control/flash.bin
+#r2 -a stm8 -n -i r2/import_flags.r2 -i r2/anal-control.r2 -m 0x8000 control/flash.bin
 r2 -a stm8 -n -i r2/anal-display.r2 -m 0x807f -s 0x00008aac display/flash.bin
 #r2 -a stm8 -n -i r2/anal-display.r2 -m 0x807f -s 0x00008f20 display/flash.bin

5e184935cf71c2a11b5fab2b95f354460365a905
Roman Valls Guimera <brainstorm@users.noreply.github.com>
2024-10-19 21:31:19 +1100

    XREFS in control board stopped working at some point, committing latest changes, could be sth related with arguments order?

--- bin/anal.sh
+++ bin/anal.sh
@@ -7,7 +7,8 @@ python3 ./bin/ghidra_labels_to_r2_flags.py
 
 # Default to control board analysis
 if [ "$1" = "display" ]; then
-    r2 -a stm8 -n -i r2/import_flags.r2 -m 0x807f -i r2/anal-display.r2 display/flash.bin
+    r2 -a stm8 -n -m 0x807f -i r2/anal-display.r2 display/flash.bin
 else
-    r2 -a stm8 -n -i r2/import_flags.r2 -m 0x8000 -i r2/anal-control.r2 control/flash.bin
+#    r2 -a stm8 -n -m 0x8000 -i r2/import_flags.r2 -i r2/anal-control.r2 control/flash.bin
+    r2 -a stm8 -n -i r2/anal-control.r2 -m 0x8000 control/flash.bin
 fi

734551de6e7288dc057394be8e7ade39616a1b92
Roman Valls Guimera <brainstorm@users.noreply.github.com>
2024-11-09 19:39:05 +1100

    Good enough fix for slides clipping bugs... still some emojis broken but 🤷 (see https://github.com/astefanutti/decktape/issues/209 and related issues for decktape)

--- bin/slides.sh
+++ bin/slides.sh
@@ -3,7 +3,7 @@
 #
 
 if [ "$1" = "pdf" ]; then
-    docker run --rm -t --net=host -v $PWD/doc:/slides astefanutti/decktape http://localhost:1948/doc/slides.md --size 1920x1080 slides.pdf
+    docker run --rm -t --net=host -v $PWD/doc:/slides ghcr.io/thomas-ferchau/decktape:pr-10 http://localhost:1948/doc/slides.md --size 1280x1024 slides.pdf
 else
     docker run --rm -p 1948:1948 -p 35729:35729 -v $PWD:/slides webpronl/reveal-md:latest /slides --watch
 fi
